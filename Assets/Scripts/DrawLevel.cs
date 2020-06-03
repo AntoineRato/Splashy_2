45c10b95cc3e60ade0c16d78095de64f986b1913
Antoine <zlino.dev@gmail.com>
2020-06-03 16:18:13 +0200

    :sparkles: Random Platform Generation added
    Random Platform Generation :
    • Platform are generated between 2 z values (for min and max), calculated previously with the GameObject GameArea.
    • For each iteration, the x value is incremented by the SpacingValue (DrawLevel.cs) and the same variable is added or substracted (chose randomly) to the z value.
    
    Platform :
    • Box collider added.
    
    Extras :
    • SimpleCameraController.cs from the URP initialization has been removed

---
+++ Assets/Scripts/DrawLevel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLevel : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    private float spacingPlatformValue = 1.35f;
+    private float z_MinGameArea = -3;
+    private float z_MaxGameArea = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x_SpawnValue = 1.35f;
+        float z_SpawnValue = -0.538f;
+        //float z_nextRandomPosition = 0f;
+
+        for (int i = 0; i < 50; i++)
+        {
+            z_SpawnValue += Random.Range(-spacingPlatformValue, spacingPlatformValue);
+            z_SpawnValue = Mathf.Min(z_MaxGameArea, Mathf.Max(z_SpawnValue, z_MinGameArea));
+
+            //z_nextRandomPosition = Random.Range(-1.35f, 1.35f);
+
+            /*if((z_SpawnValue + z_nextRandomPosition) > z_MaxGameArea)
+            {
+                z_SpawnValue = z_MaxGameArea;
+            }*/
+            Instantiate(platformPrefab, new Vector3(x_SpawnValue, 0, z_SpawnValue), Quaternion.identity);
+
+            x_SpawnValue += spacingPlatformValue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

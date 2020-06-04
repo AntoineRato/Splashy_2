0e5a3ccf017d372bd0f3b398c72680b12d02e5f3
Antoine <zlino.dev@gmail.com>
2020-06-04 17:56:26 +0200

    :sparkles: Ball movements control added
    Ball :
    • Now you can move the ball by swiping your finger at horizontal.

---
+++ Assets/Scripts/BallMovements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovements : MonoBehaviour
+{
+    private Touch touch;
+    private float moveSpeed = 0.007f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - touch.deltaPosition.x * moveSpeed);
+            }
+        }
+    }
+}

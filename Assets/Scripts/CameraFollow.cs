1cd3dce4a3a80e1b2be535b656b1a4201e8f13fa
Antoine <zlino.dev@gmail.com>
2020-06-04 17:54:07 +0200

    :sparkles: Camera follow the ball
    Camera :
    • Now follow the ball on the z axis.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform ball;
+	public float smoothSpeed = 0.125f;
+
+	void FixedUpdate()
+	{
+		Vector3 desiredPosition = new Vector3(this.transform.position.x, this.transform.position.y, ball.position.z);
+		transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+	}
+}

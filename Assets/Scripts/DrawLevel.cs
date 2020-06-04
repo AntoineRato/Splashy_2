567c5391f7feee7b15728230739cb77a50ed3638
Antoine <zlino.dev@gmail.com>
2020-06-04 17:53:09 +0200

    :bento: Asset to show in game FPS added
    FPS Counter :
    https://assetstore.unity.com/packages/tools/gui/graphy-ultimate-fps-counter-stats-monitor-debugger-105778

--- Assets/Scripts/DrawLevel.cs
+++ Assets/Scripts/DrawLevel.cs
@@ -11,6 +11,11 @@ public class DrawLevel : MonoBehaviour
     private float z_minGameArea = -3;
     private float z_maxGameArea = 2;
 
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
     // Start is called before the first frame update
     void Start()
     {

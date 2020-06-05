7fc31b6e6a0a2fc913e199e48299f12f9de308dc
Antoine <zlino.dev@gmail.com>
2020-06-05 22:44:27 +0200

    :sparkles: Now there is a chance to spawn a second platform

--- Assets/Scripts/DrawLevel.cs
+++ Assets/Scripts/DrawLevel.cs
@@ -9,9 +9,9 @@ public class DrawLevel : MonoBehaviour
     public GameObject platformPrefab;
     public GameObject lastPlatformPrefab;
 
-    private float spacingPlatformValue = 1.35f;
-    private float z_minGameArea = -3;
-    private float z_maxGameArea = 2;
+    private float spacingPlatformValue = 1.70f;
+    private float z_minGameArea = -5;
+    private float z_maxGameArea = 5;
 
 #if UNITY_EDITOR
     public float Timescale = 1.5f;
@@ -20,7 +20,7 @@ public class DrawLevel : MonoBehaviour
     private void Awake()
     {
         gameIsRunning = false;
-        Application.targetFrameRate = 300;
+        Application.targetFrameRate = 600;
         Time.timeScale = 1.5f;
     }
 
@@ -30,12 +30,38 @@ public class DrawLevel : MonoBehaviour
         float x_spawnValue = 1.35f;
         float z_spawnValue = -0.538f;
 
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 200; i++)
         {
             z_spawnValue += Random.Range(-spacingPlatformValue, spacingPlatformValue);
             z_spawnValue = Mathf.Min(z_maxGameArea, Mathf.Max(z_spawnValue, z_minGameArea));
 
             Instantiate(platformPrefab, new Vector3(x_spawnValue, 0, z_spawnValue), Quaternion.identity);
+            
+            // 40% chance to have a second platform spawn
+            if(Random.value <= 0.25f)
+            {
+                //float z_bonusSpawn = z_spawnValue;
+                float z_bonusSpawn = 0;
+                if ((z_bonusSpawn + 1.35f) > z_maxGameArea)
+                {
+                    z_bonusSpawn += Random.Range((-spacingPlatformValue - 1.3f), -1.35f);
+                    z_bonusSpawn = Mathf.Min(z_maxGameArea, Mathf.Max(z_bonusSpawn, z_minGameArea));
+                }
+                else if((z_bonusSpawn - 1.35f) < z_minGameArea)
+                {
+                    z_bonusSpawn += Random.Range(1.35f, (spacingPlatformValue + 1.3f));
+                    z_bonusSpawn = Mathf.Min(z_maxGameArea, Mathf.Max(z_bonusSpawn, z_minGameArea));
+                }
+                else
+                {
+                    z_bonusSpawn += Random.Range(1.35f, (spacingPlatformValue + 1.3f));
+                    if (Random.value <= 0.5f)
+                        z_bonusSpawn *= -1;
+                    z_bonusSpawn = Mathf.Min(z_maxGameArea, Mathf.Max(z_bonusSpawn, z_minGameArea));
+                }
+
+                Instantiate(platformPrefab, new Vector3(x_spawnValue, 0, (z_spawnValue + z_bonusSpawn)), Quaternion.identity);
+            }
 
             x_spawnValue += spacingPlatformValue;
         }
